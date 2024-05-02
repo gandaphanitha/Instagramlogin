a7f769edd63c251d9c55cee40feebaf05d2da63a
gandaphanitha <161686624+gandaphanitha@users.noreply.github.com>
2024-05-02 10:32:25 +0530

    Update insta.js

--- insta.js
+++ insta.js
@@ -2,17 +2,10 @@ function login() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*"];
-    if (username.includes("@") || username.includes(".")){
+    if (!username.includes("@") || !username.includes(".")){
         console.log("Enter valid email");
         return;
     }
-
-    if (password.length < 8) {
-        console.log("Password is too short. It must be at least 8 characters long.");
-        return;
-    }
-
-    
     let containsSpecialChar = false;
     for (let i = 0; i < password.length; i++) {
         if (specialChars.includes(password[i])) {
@@ -20,14 +13,10 @@ function login() {
             break;
         }
     }
-
     if (!containsSpecialChar) {
         console.log("Password must contain at least one special character.");
         return;
     }
-
     console.log("Username: " + username);
     console.log("Password: " + password);
 }
-
-  
